1c6ecd28af1f1f5ed7281971173a5b54837641ab
liuqiang <qiang.liu16@pactera.com>
2020-04-22 12:06:27 +0800

    上传 Jcenter

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # PermissionX
-运行时权限库
+Android 运行时权限库
+
+## 引入
+
+```
+dependencies {
+    implementation 'com.chronglau.library:permissionx:1.0.0'
+}
+
+```
+
+## 使用
+
+```
+PermissionX.request(this, Manifest.permission.CALL_PHONE) { allGranted, deniedList ->
+    if (allGranted) {
+        // call
+    } else {
+        Toast.makeText(this, "you denied the permission", Toast.LENGTH_SHORT).show()
+    }
+}
+
+```
+
+
--- app/src/main/java/com/chrongliu/permissionx/MainActivity.kt
+++ app/src/main/java/com/chrongliu/permissionx/MainActivity.kt
@@ -1,12 +1,41 @@
 package com.chrongliu.permissionx
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.chrongliu.library.PermissionX
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnCall.setOnClickListener {
+            PermissionX.request(this, Manifest.permission.CALL_PHONE) { allGranted, deniedList ->
+                if (allGranted) {
+                    callPhone()
+                } else {
+                    Toast.makeText(this, "you denied the permission", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun callPhone() {
+        try {
+            val intent = Intent(Intent.ACTION_CALL)
+            intent.data = Uri.parse("tel:10086")
+            startActivity(intent)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
     }
 }
